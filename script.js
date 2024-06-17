35c66a46f8fdd5de57e599c5f2e71cff08e61f19
Arruda Junior <juniorarruda15@gmail.com>
2024-06-17 17:49:26 -0300

    Async/Await resolvendo problema de callback

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-.then(res =>res.json())
-.then((videos) => 
-    videos.forEach((video => {
+
+async function buscarEMostrarVideos(){
+    const busca = await fetch("http://localhost:3000/videos");
+    const videos =  await busca.json();
+
+    videos.forEach((video) => {
         containerVideos.innerHTML += `
         <li class="videos__item">
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
@@ -14,9 +16,8 @@ const api = fetch("http://localhost:3000/videos")
             </div>
         </li>
         `;
-    }))
-    
-)
-.catch((error) => {
-    containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`
-})
+    })
+}
+
+buscarEMostrarVideos();
+
